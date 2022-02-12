6c84743e7e8a7bc5d93a77426250012b675ecce8
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-02-12 17:13:20 +0530

    sql with pandas

--- demo.sql
+++ demo.sql
@@ -27,3 +27,29 @@ delete from students where roll_no=3;
 
 -- adding a primary key
 alter table students add primary key(roll_no);
+
+
+
+-- create a table countries
+create table countries (c_id int primary key, country varchar(50), capital varchar(50));
+
+-- insert some values
+insert into countries values (1, "India", "New Delhi");
+insert into countries values (2, "USA", "Washington DC");
+insert into countries values (3, "Canada", "Ottawa");
+insert into countries values (4, "Thailand", "Bangkok");
+insert into countries values (5, "South Korea", "Seoul");
+
+-- order by
+select * from countries order by country; -- ascending order
+select * from countries order by country desc; -- descending order
+
+-- aggregate functions
+select count(c_id) from countries;
+select count(c_id) from countries where c_id > 3;
+select sum(c_id) from countries;
+
+-- like
+select * from countries where country like '___';
+select * from countries where country like '%a';
+select * from countries where country like '_n%';
