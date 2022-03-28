c9973bc29bf44191b5da08d4ca7ecf2a45991579
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-28 17:20:13 +0530

    api with html

--- machine learning/api/.ipynb_checkpoints/app-checkpoint.py
+++ machine learning/api/.ipynb_checkpoints/app-checkpoint.py
@@ -1,6 +1,7 @@
 # Importing the libraries
-from flask import Flask
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 
 # Global variables
@@ -11,22 +12,28 @@ loaded_model = pickle.load(open('Model.pkl', 'rb'))
 # User defined routes
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("index.html")
 
-@app.route("/prediction")
+@app.route("/prediction", methods=['POST'])
 def predict():
-    glucose = 150
-    bmi = 25
-    age = 40
+    glucose = request.form["glucose"]
+    bmi = request.form["bmi"]
+    age = request.form["age"]
+    
     prediction = loaded_model.predict([[glucose, bmi, age]])[0]
+    probability = loaded_model.predict_proba([[glucose, bmi, age]])
+    probability = np.round((np.max(probability) * 100), 2)
     output = ""
+    probability = f"{probability}%"
 
     if prediction == 0:
         output = "Not Diabetic"
     else:
         output = "Diabetic"
+        
+    print(prediction, probability)
 
-    return "Prediction: " + output
+    return render_template("index.html", output_prediction=output, output_proba=probability)
 
 
 # Main function
