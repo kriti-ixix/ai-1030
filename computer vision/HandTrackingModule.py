60436ce0d8cffc1c6ffa7fdde830f0425f979204
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-05-30 17:31:43 +0530

    Update HandTrackingModule.py

--- computer vision/HandTrackingModule.py
+++ computer vision/HandTrackingModule.py
@@ -19,11 +19,33 @@ class HandDetector:
         self.results = self.hands.process(image)
         
         if self.results.multi_hand_landmarks:
-            print(self.results.multi_hand_landmarks)
             for handLms in self.results.multi_hand_landmarks:
                 self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
                 
         return image
+    
+    def find_position(self, image, handNo=0):
+        self.lmList = []
+        
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                
+                cv2.circle(image, (cx, cy), 8, (255, 0, 255), -1)
+        
+        return self.lmList
+    
+    def fingers_up(self):
+        fingers = []
+        
+        # Thumb
+        
+        
+        # Four fingers
     
     
 cap = cv2.VideoCapture(0)
@@ -32,6 +54,8 @@ detector = HandDetector()
 while True:
     _, frame = cap.read()
     image = detector.find_hands(frame)
+    lmList = detector.find_position(image)
+    print(lmList)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
     cv2.imshow("Hands", image)
